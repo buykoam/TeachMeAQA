6309826de5f7764c290cc49282c3ecd837008aa0
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-04-30 12:39:54 +0300

    add new file

---
+++ src/homework2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class homework2 {
+    public static void main(String[] args) {
+        System.out.println("    |    |    ");
+        System.out.println("____|____|____");
+        System.out.println("    |    |    ");
+        System.out.println("____|____|____");
+        System.out.println("    |    |    ");
+        System.out.println("    |    |    ");
+
+    }
+}

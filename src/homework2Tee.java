ff2c12ad0ba1bb55a1135aa530d7509241d9db12
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-04-30 12:45:37 +0300

    add new file

---
+++ src/homework2Tee.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class homework2Tee {
+    public static void main(String[] args) {
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+    }
+}

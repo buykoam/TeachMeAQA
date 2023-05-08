5fa30d82aa615b3bc79f356283f9a1a786a4ac25
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-05-08 16:43:02 +0300

    add new file

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,63 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        getInformation();
+        initializeVariables();
+        getBMI();
+    }
+    public static void getInformation() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите имя:");
+        String name = in.nextLine();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите фамилию:");
+        String surname = sc.nextLine();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner as = new Scanner(System.in);
+        System.out.println("Введите возраст:");
+        int age = as.nextInt();
+
+        Scanner aw = new Scanner(System.in);
+        System.out.println("Введите рост:");
+        int height = aw.nextInt();
+
+        Scanner aq = new Scanner(System.in);
+        System.out.println("Введите вес:");
+        int weight = aq.nextInt();
+
+        System.out.println("Name: " + name );
+        System.out.println("Surname: " + surname);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+    }
+    public static void initializeVariables() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите символ:");
+        char mySymbol = in.next().charAt(0);
+        int x = mySymbol;
+
+        System.out.println(mySymbol + " - " + x);
+    }
+    public static void getBMI() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите рост:");
+        int height = in.nextInt();
+
+        Scanner aq = new Scanner(System.in);
+        System.out.println("Введите вес:");
+
+        double weight = aq.nextDouble();
+        double height1 = height  / 100;
+        double height2 = Math.pow(height1,2);
+        double BMI = weight / height2;
+
+        System.out.println("Result: " + BMI);
+
+
     }
 }

fe7a182e16280b8bb523047e9fc7b555c3ff53cb
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-05-10 23:13:14 +0300

    fix code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        getNameSeason();
+        //getNameSeason();
+        //getSeasonTwoMethod();
+        getEvenlyOddNumber();
     }
 
     public static String getNameSeason()
@@ -38,5 +41,54 @@ public class Main {
         }
         System.out.println("Message: " + Season);
         return Season;
+    }
+
+    public static String getSeasonTwoMethod() {
+        Scanner z = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int number = z.nextInt();
+
+        if (number == 1)
+        {
+            System.out.println("Winter");
+        }
+
+        if (number == 2)
+        {
+            System.out.println("Spring");
+        }
+
+        if (number == 3)
+        {
+            System.out.println("Summer");
+        }
+
+        if (number == 4)
+        {
+            System.out.println("Autumn");
+        }
+
+        else
+        {
+            System.out.println("Sorry please try another Number");
+        }
+        return null;
+    }
+
+    public static void getEvenlyOddNumber()
+    {
+        Scanner x = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int number = x.nextInt();
+
+        if (number % 2)
+        {
+            System.out.println("Четное");
+        }
+
+        else
+        {
+            System.out.println("Нечетное");
+        }
     }
 }

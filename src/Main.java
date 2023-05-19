4671c38f1ee6d18e11931df899cc56ee40c0e2d3
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-05-19 21:47:13 +0300

    add code

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,57 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         getBoard();
+        getCountNumber();
     }
-// Number 1
+
+    // Number 0
+    public static void getCountNumber() {
+            int[][][] cube = new int[][][]{
+                    {
+                            {2, 4, 6},
+                            {7, 9, 1},
+                            {8, 3, 5}
+                    }
+            };
+            for (int j = 0; j < cube.length; j++) {
+                for (int y = 0; y < cube[j].length; y++) {
+                    for (int z = 0; z < cube[j][y].length; z++) {
+                        System.out.print( " - " + cube[j][y][z]);
+                    }
+                    System.out.println();
+                }
+            }
+            Scanner enter = new Scanner(System.in);
+            System.out.print("Please, enter your number: ");
+            int increment = enter.nextInt();
+            for (int j = 0; j < cube.length; j++) {
+                for (int y = 0; y < cube[j].length; y++) {
+                    for (int z = 0; z < cube[j][y].length; z++) {
+                        cube[j][y][z] += increment;
+                    }
+                }
+            }
+            System.out.println("New array after change: ");
+            for (int j = 0; j < cube.length; j++) {
+                for (int y = 0; y < cube[j].length; y++) {
+                    for (int z = 0; z < cube[j][y].length; z++) {
+                        System.out.print(" - " + cube[j][y][z]);
+                    }
+                    {
+                        System.out.println();
+                    }
+                }
+                System.out.println();
+            }
+        }
+
+    // Number 1
     public static void getBoard() {
-        String [][] board = {{"W","B","W","B","W","B","W","B"}, {"B","W","B","W","B","W","B","W"}, {"W","B","W","B","W","B","W","B"}, {"B","W","B","W","B","W","B","W"}, {"W","B","W","B","W","B","W","B"}, {"B","W","B","W","B","W","B","W"}, {"W","B","W","B","W","B","W","B"}, {"B","W","B","W","B","W","B","W"} };//объявили массив и заполнили его элементами
+        String[][] board = {{"W", "B", "W", "B", "W", "B", "W", "B"}, {"B", "W", "B", "W", "B", "W", "B", "W"}, {"W", "B", "W", "B", "W", "B", "W", "B"}, {"B", "W", "B", "W", "B", "W", "B", "W"}, {"W", "B", "W", "B", "W", "B", "W", "B"}, {"B", "W", "B", "W", "B", "W", "B", "W"}, {"W", "B", "W", "B", "W", "B", "W", "B"}, {"B", "W", "B", "W", "B", "W", "B", "W"}};//объявили массив и заполнили его элементами
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 System.out.print(" " + board[i][j] + " ");
@@ -15,3 +60,6 @@ public class Main {
         }
     }
 }
+
+
+

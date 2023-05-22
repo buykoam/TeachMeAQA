62396e14072b7981029ef71d607e984c5bc50c06
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-05-22 17:34:33 +0300

    add code

--- src/Main.java
+++ src/Main.java
@@ -1,110 +1,122 @@
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        getAnswerForNumbers();
-        getArray();
-        getMaxMinNumber();
-        getMaxOrAverageElements();
-    }
-
-    // Number 0
-    public static void getAnswerForNumbers() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter number");
-        int number[] = {1, 2, 3, 4, 5, 6};
-        int i = input.nextInt();
-        if (i <= number[0] || i <= number[1] || i <= number[2] || i <= number[3] || i <= number[4] || i <= number[5]) {
-            System.out.println(i + " number include in array");
-        } else {
-            System.out.println("number not include in array");
+        public static void main(String[] args) {
+            getAnswerForNumbers();
+            getArray();
+            getMaxMinAvgNumber();
+            getMaxOrAverageElements();
         }
-    }
 
-    // Number 1
-    public static void getArray() {
-        int[] array = new int[]{5, 8, 19, 63, 54};
-        Scanner enter = new Scanner(System.in);
-        System.out.print("Please, enter your number: ");
-        int number = enter.nextInt();
-        boolean found = false;
-        int newSize = 0;
-        for (int i = 0; i < array.length; i++)
-            if (array[i] == number) {
-                found = true;
+        // Number 0
+        public static void getAnswerForNumbers() {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter number: ");
+            int number[] = {1, 2, 10, 4, 5, 6, 10, 15};
+            int i = input.nextInt();
+
+            int counter = 0;
+
+            for (int el = 0; el < number.length; el++) {
+                if (number[el] == i) {
+                    System.out.println("There is " + i +
+                            " in the array at index: " + el);
+                    counter++;
+                }
+            }
+
+            if (counter == 0) {
+                System.out.println("There is no such number in the array.");
+            }
+        }
+            // Number 1
+        public static void getArray()
+        {
+            int[] array = new int[]{5, 8, 19, 63, 54};
+            Scanner enter = new Scanner(System.in);
+            System.out.print("Please, enter your number: ");
+            int number = enter.nextInt();
+            boolean found = false;
+            int newSize = 0;
+            for (int i = 0; i < array.length; i++)
+                if (array[i] == number) {
+                    found = true;
+                } else {
+                    array[newSize] = array[i];
+                    newSize++;
+                }
+            if (found) {
+                int[] newArray = new int[newSize];
+                System.arraycopy(array, 0, newArray, 0, newSize);
+                System.out.println("array elements after removal: ");
+
+                for (int i = 0; i < newArray.length; i++) {
+                    System.out.println(newArray[i]);
+                }
+
             } else {
-                array[newSize] = array[i];
-                newSize++;
+                System.out.println("Number not found in array");
             }
-        if (found) {
-            int[] newArray = new int[newSize];
-            System.arraycopy(array, 0, newArray, 0, newSize);
-            System.out.println("array elements after removal: ");
+        }
 
-            for (int i = 0; i < newArray.length; i++) {
-                System.out.println(newArray[i]);
+        // Number 2
+        public static void getMaxMinAvgNumber() {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter amount array:");
+            int kol = in.nextInt();
+            double[] array = new double[kol];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = Math.random();
+        }
+            double max = array[0]; // Массив не должен быть пустым
+            double min = array[0];
+            double avg = 0;
+            for (int i = 0; i < array.length; i++) {
+                if(max < array[i])
+                    max = array[i];
+                if(min > array[i])
+                    min = array[i];
+                avg += array[i]/array.length;
             }
 
-        } else {
-            System.out.println("Number not found in array");
+            System.out.println("max = " + max);
+            System.out.println("min = " + min);
+            System.out.println("avg = " + avg);
+        }
+
+        // Number 3
+        public static void getMaxOrAverageElements() {
+            int[] mas1 = new int[5], mas2 = new int[5];
+            int s1, s2;
+            double a1, a2;
+            s1 = 0;
+            s2 = 0;
+            for (int i = 0; i < mas1.length; i++) {
+                mas1[i] = (int) (Math.random() * 6);
+                mas2[i] = (int) (Math.random() * 6);
+            }
+            for (int i = 0; i < mas1.length; i++) {
+                System.out.print(mas1[i] + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < mas2.length; i++) {
+                System.out.print(mas2[i] + " ");
+            }
+            for (int i = 0; i < mas1.length; i++) {
+                s1 = s1 + mas1[i];
+            }
+            for (int i = 0; i < mas2.length; i++) {
+                s2 = s2 + mas2[i];
+            }
+            System.out.println();
+            a1 = (double) s1 / 5;
+            a2 = (double) s2 / 5;
+            if (a1 < a2) {
+                System.out.println("The arithmetic mean of the first array is less than the arithmetic mean of the second array");
+            } else if (a2 < a1) {
+                System.out.println("The arithmetic mean of the second array is less than the arithmetic mean of the first array");
+            } else {
+                System.out.println("Arithmetic means are equal to each other");
+            }
         }
     }
-
-    // Number 2
-    public static void getMaxMinNumber() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter amount array:");
-        int kol = in.nextInt();
-        int Arr[] = new int[kol];
-        int max = Arr[0], min = Arr[0];
-        for (int i = 0; i < Arr.length; i++) {
-            System.out.print("Enter " + i + " element array:");
-            Arr[i] = in.nextInt();
-            if (max < Arr[i])
-                max = Arr[i];
-            if (min > Arr[i])
-                min = Arr[i];
-        }
-        System.out.println("max=" + max);
-        System.out.println("min=" + min);
-    }
-
-    // Number 3
-    public static void getMaxOrAverageElements() {
-        int[] mas1 = new int[5], mas2 = new int[5];
-        int s1, s2;
-        double a1, a2;
-        s1 = 0;
-        s2 = 0;
-        for (int i = 0; i < mas1.length; i++) {
-            mas1[i] = (int) (Math.random() * 6);
-            mas2[i] = (int) (Math.random() * 6);
-        }
-        for (int i = 0; i < mas1.length; i++) {
-            System.out.print(mas1[i] + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < mas2.length; i++) {
-            System.out.print(mas2[i] + " ");
-        }
-        for (int i = 0; i < mas1.length; i++) {
-            s1 = s1 + mas1[i];
-        }
-        for (int i = 0; i < mas2.length; i++) {
-            s2 = s2 + mas2[i];
-        }
-        System.out.println();
-        a1 = (double) s1 / 5;
-        a2 = (double) s2 / 5;
-        if (a1 < a2) {
-            System.out.println("The arithmetic mean of the first array is less than the arithmetic mean of the second array");
-        } else if (a2 < a1) {
-            System.out.println("The arithmetic mean of the second array is less than the arithmetic mean of the first array");
-        } else {
-            System.out.println("Arithmetic means are equal to each other");
-        }
-    }
-}
-

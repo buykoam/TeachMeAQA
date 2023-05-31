34ab2b3d04e1db788eb380a27f4a05b20cf713a1
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-05-31 19:17:16 +0300

    add code

--- src/Exceptions/Main.java
+++ src/Exceptions/Main.java
@@ -1,37 +1,4 @@
 package Exceptions;
-import java.util.Scanner;
+
 public class Main {
-
-    private static String massage = "Incorrect password";
-    private static String massage2 = "Incorrect login";
-
-    public static void main(String[] args) {
-        System.out.println("Please enter your login: ");
-        String login = enter().nextLine();
-        System.out.println("Please enter your password: ");
-        String password = enter().nextLine();
-        System.out.println("Please confirm your password: ");
-        String confirmPassword = enter().nextLine();
-    }
-
-    public static Scanner enter() {
-        return new Scanner(System.in);
-    }
-
-    static void getValidarion(String login, String password, String confirmPassword)
-            throws WrongPasswordException, WrongLoginException {
-        if (login.contains(" ") | login.length() > 20) {
-            throw new WrongLoginException(massage2);
-        }
-        {
-            if (password.contains(" ") | password.length() > 20) {
-                throw new WrongPasswordException(massage);
-            }
-            if (!confirmPassword.equals(password)) {
-                throw new WrongPasswordException();
-            }
-        }
-    }
 }
-
-

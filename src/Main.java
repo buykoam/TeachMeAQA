284d35e7303aea028d32518aa2308a0da6fadc79
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-06-11 20:08:24 +0300

    add code

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws FileNotFoundException {
+        String path = "D:\\TeachMeAQA\\TeachMeAQA\\src\\Name.txt";
+        File file = new File(path);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner scanner = new Scanner(file);
+        ArrayList<String> list = new ArrayList<String>();
+        while (scanner.hasNextLine()) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            list.add(scanner. nextLine());
+
+            System.out.println(scanner.nextLine());
+
         }
+        scanner.close();
     }
 }
